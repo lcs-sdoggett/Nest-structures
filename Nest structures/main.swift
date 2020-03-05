89486b3f282bf10efc6e010d79eacb76308cb544
Scott Doggett <sdoggett@lcs.on.ca>
2020-03-05 15:28:44 -0500

    Initial Commit

---
+++ Nest structures/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  Nest structures
+//
+//  Created by Doggett, Scott on 2020-03-05.
+//  Copyright © 2020 Doggett, Scott. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
